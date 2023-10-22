078a3cd09b8ef458deb422a62c0e58e2cd912fbe
Juan Manuel Elosegui <elosegui@gmail.com>
2023-10-22 19:43:39 -0400

    Adding powershell profiles as symbolic links

--- Setup.ps1
+++ Setup.ps1
@@ -3,21 +3,34 @@
 $mapping = @(
    @{
         destination = "$env:LOCALAPPDATA\nvim"
-        source = "$PWD\nvim"
+        source = "$PSScriptRoot\nvim"
     },
     @{
         destination = "$env:USERPROFILE\jmelosegui-omp.json"
-        source = "$PWD\oh-my-posh\jmelosegui-omp.json"
+        source = "$PSScriptRoot\oh-my-posh\jmelosegui-omp.json"
     },
     @{
         destination = "$env:USERPROFILE\AppData\Roaming\Code\User\settings.json"
-        source = "$PWD\vscode\settings.json"
+        source = "$PSScriptRoot\vscode\settings.json"
     },
     @{
         destination = "$env:USERPROFILE\AppData\Roaming\Code\User\keybindings.json"
-        source = "$PWD\vscode\keybindings.json"
+        source = "$PSScriptRoot\vscode\keybindings.json"
+    },
+    @{
+        destination = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+        source = "$PSScriptRoot\powershell\profile.ps1"
+    },
+    @{
+        destination = "$env:USERPROFILE\Documents\PowerShell\Microsoft.VSCode_profile.ps1"
+        source = "$PSScriptRoot\powershell\profile.ps1"
+    },
+    @{
+        destination = "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+        source = "$PSScriptRoot\powershell\profile.ps1"
     }
 )
+
 
 foreach ($map in $mapping) {
     if (Test-Path $map.destination) {
@@ -28,9 +41,3 @@ foreach ($map in $mapping) {
     }
 }
 
-# Override the default profile for PowerShell 5.x
-Get-Content "$PWD\powershell\profile.ps1" | Set-Content $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-# Override the default profile for PowerShell within VSCode
-Get-Content "$PWD\powershell\profile.ps1" | Set-Content $env:USERPROFILE\Documents\PowerShell\Microsoft.VSCode_profile.ps1
-# Override the default profile for PowerShell 6.x
-Get-Content "$PWD\powershell\profile.ps1" | Set-Content $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
